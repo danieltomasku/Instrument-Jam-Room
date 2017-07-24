0e9ae3da6539f38e4e27579dd8537ffda9735e06
Kevin Haugen <kevin.haugen@razorfish.com>
2017-07-24 14:29:40 -0700

    socket io working

--- client/js/app.js
+++ client/js/app.js
@@ -1,11 +1,21 @@
 'use strict';
 
 var ready = require('document-ready');
-var socket = require('socket.io-client');
+var socket = require('socket.io-client')('localhost:3000');
 
 var app = {
 	init() {
-		socket.connect('localhost:3000');
+		socket.on('connect', function () {
+		    console.log('You are connected');
+		});
+
+		socket.on('newUser', function (data) {
+		    console.log(data);
+		});
+
+		socket.on('buttonPress', function (data) {
+		    console.log(data);
+		});
 	}
 };
 
