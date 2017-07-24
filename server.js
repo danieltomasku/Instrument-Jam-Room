da8e26dc96c1315fa53ff3a4eb7c49d17af9aac1
Daniel Ku <danielku@danielku.hsd1.or.comcast.net>
2017-07-24 12:24:00 -0700

    Initial setup of CSS

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var path = require('path');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var port = 3000;
+
+app.use('/', express.static(__dirname + '/client'));
 
 io.on('connection', function(){
 	console.log('Socket connected');
