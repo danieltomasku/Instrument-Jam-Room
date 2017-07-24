fdc4e135377224dbe4c78c64091e7bf34c647090
Kevin Haugen <kevin.haugen@razorfish.com>
2017-07-24 12:24:37 -0700

    socket server

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
 'use strict';
 
 var path = require('path');
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var port = 3000;
 
+app.use('/', express.static(__dirname + '/client'));
+
+// Button Server
+var buttonSocket = require('socket.io-client')('http://10.0.0.145:3000/');
+
+buttonSocket.on('connect', function() {
+	console.log('connected to button socket');
+});
+
+buttonSocket.on('buttonUpdate', function(data) {
+	if (data.id === 6) {
+		console.log(data);
+	}
+});
+
+buttonSocket.on('disconnect', function(){
+	console.log('Button socket disconnected');
+});
+
+buttonSocket.on('connect_error', function (error) {
+  console.log('Error', error);
+});
+
 io.on('connection', function(){
-	console.log('Socket connected');
+	console.log('User connected');
 });
 
 app.get(['*'], function (req, res) {
-  res.sendFile(path.resolve(__dirname + '/client', 'index.html'));
+	// buttonSocket.open();
+  	res.sendFile(path.resolve(__dirname + '/client', 'index.html'));
 });
 
 server.listen(port);
